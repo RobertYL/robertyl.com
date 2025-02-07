edc1b548bf02cf133e4f15adf4cac0e9c45bc151
Robert Lee <rob3rtyl33@gmail.com>
2025-02-07 14:15:28 -0500

    Removed docs/

--- docs/js/new-tab.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function () {
-    const links = document.querySelectorAll('a[href^="https://"], a[href^="http://"], a[href^="/assets"]')
-    links.forEach(link => {
-        link.setAttribute("target", "_blank")
-        link.setAttribute("rel", "noopener noreferrer")
-    })
-})()
